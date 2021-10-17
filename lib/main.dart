31262e82e9b8cdebdb7bb643eba1878e8eccc954
Md. Abul Hasnat <70832189+ahahasanat@users.noreply.github.com>
2021-10-17 20:23:11 +0600

    add drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,97 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:Profile(),
-    )
-    );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Profile(),
+  ));
 }
+
 class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+        backgroundColor: Colors.teal[300],
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Image.asset("bg.jpg"),
+                Positioned(
+                  left: 20,
+                  top: 10,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CircleAvatar(
+                        radius: 32,
+                        backgroundImage: AssetImage("2.jpg"),
+                      ),
+                      Text(
+                        "Md. Abul Hasnat",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                        ),
+                      ),
+                      Text(
+                        "abulhasnat719@gmail.com",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            ListTile(
+              onTap: () {},
+              leading: Icon(Icons.music_note),
+              title: Text("Music"),
+            ),
+            ListTile(
+              onTap: () {},
+              leading: Icon(Icons.movie),
+              title: Text("Movie"),
+            ),
+            ListTile(
+              onTap: () {},
+              leading: Icon(Icons.settings),
+              title: Text("setings"),
+            ),
+            ListTile(
+              leading: Icon(Icons.contacts),
+              title: Text("Contact Us"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                width: double.infinity,
+                height: 48,
+                child: Row(
+                  children: [
+                    Icon(Icons.sd_card_alert_rounded),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text("Sd_Card")
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
       backgroundColor: Colors.teal,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
@@ -20,89 +100,91 @@ class Profile extends StatelessWidget {
           children: [
             Container(
               child: CircleAvatar(
-                radius:50,
+                radius: 50,
                 backgroundImage: AssetImage('assets/pp.jpg'),
               ),
             ),
             Text(
               "Md. Abul Hasnat",
               style: TextStyle(
-                fontFamily: 'Pacifico',
-                fontSize: 28,
-                fontWeight: FontWeight.w700,
-                color: Colors.white
-              ),
+                  fontFamily: 'Pacifico',
+                  fontSize: 28,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white),
             ),
             Text(
               'FLUTTER DEVELOPER',
-            style: TextStyle(
-              //fontWeight:FontWeight.w700,
-              letterSpacing: 3,
-              fontSize:16,
-              color: Colors.white,
-
-            ),
+              style: TextStyle(
+                //fontWeight:FontWeight.w700,
+                letterSpacing: 3,
+                fontSize: 16,
+                color: Colors.white,
+              ),
             ),
             Divider(
-              color:Colors.white.withOpacity(0.75),
+              color: Colors.white.withOpacity(0.75),
               thickness: 1,
               indent: 150,
               endIndent: 150,
             ),
-            Container(
-              width:double.infinity,
-              padding:EdgeInsets.symmetric(horizontal: 20),
-              height:48,
-              decoration:BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color:Colors.white,
-                 ),
-                 child: Row(
-                children: [
-                  Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  SizedBox(width: 28,),
-                  Text(
-                    "+8801773365506",
-                    style: TextStyle(
-                      color: Colors.teal,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500
-                    ),
-                  ),
-                  ],
-                 ),),
-                   SizedBox(height: 20,),
             Container(
               width: double.infinity,
               padding: EdgeInsets.symmetric(horizontal: 20),
               height: 48,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(8),
-                color: Colors.white
+                color: Colors.white,
               ),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 28,
+                  ),
+                  Text(
+                    "+8801773365506",
+                    style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              height: 48,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8), color: Colors.white),
               child: Row(
                 children: [
                   Icon(
                     Icons.email,
                     color: Colors.teal,
                   ),
-                  SizedBox(width: 28,),
+                  SizedBox(
+                    width: 28,
+                  ),
                   Text(
                     "abulhasnat719@gmail.com",
                     style: TextStyle(
-                      color: Colors.teal,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500
-                    ),
+                        color: Colors.teal,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500),
                   ),
                 ],
               ),
             ),
           ],
-
         ),
-      ),);
-  }}
+      ),
+    );
+  }
+}
